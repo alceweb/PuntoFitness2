9bd2833d55b352771e4b071c4793dd450ae76ed1
almissh <almissh@gmail.com>
2015-08-31 17:26:07 +0200

    commentato il reindirizzamento a mobile master

--- PuntoFitness2/ViewSwitcher.ascx.cs
+++ PuntoFitness2/ViewSwitcher.ascx.cs
@@ -21,10 +21,10 @@ namespace PuntoFitness2
         {
             // Determine current view
             var isMobile = WebFormsFriendlyUrlResolver.IsMobileView(new HttpContextWrapper(Context));
-            CurrentView = isMobile ? "Mobile" : "Desktop";
-
+          //  CurrentView = isMobile ? "Mobile" : "Desktop";
+            CurrentView = "Desktop";
             // Determine alternate view
-            AlternateView = isMobile ? "Desktop" : "Mobile";
+           // AlternateView = isMobile ? "Desktop" : "Mobile";
 
             // Create switch URL from the route, e.g. ~/__FriendlyUrls_SwitchView/Mobile?ReturnUrl=/Page
             var switchViewRouteName = "AspNet.FriendlyUrls.SwitchView";
